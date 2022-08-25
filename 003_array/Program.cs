41c5cdb5865404d197271ea2d09deb97ae5a6b03
Roman <romangushchintlt@gmail.com>
2022-08-25 23:27:50 +0400

    modify work 003

--- 003_array/Program.cs
+++ 003_array/Program.cs
@@ -1,19 +1,70 @@
 ﻿//пример поиска индекса по значению без методов и с константами
-int [] array = {1,12,31,18,4,15,16,17,18};
+// int [] array = {1,12,31,18,4,15,16,17,18};
 
-int n = array.Length;
-int find = 18;
+// int n = array.Length;
+// int find = 18;
 
-int index = 0;
+// int index = 0;
 
-while(index < n)
+// while(index < n)
+// {
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     //index = index + 1;
+//     index++;
+// }
+
+
+// пример с использованием массива с методами и без констант
+void FillArray(int[] collection)
+
 {
-    if(array[index] == find)
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
     {
-        Console.WriteLine(index);
-        break;
+        collection[index] = new Random().Next(1,10);
+        index++;
     }
-    //index = index + 1;
-    index++;
 }
 
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index<count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10]; //создай новый массив с 10 числами - будут заполнены нулями
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
