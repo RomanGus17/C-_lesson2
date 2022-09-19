cc64fe6b51f13d3dd2144f62e5e8b575b703a3fe
Roman <romangushchintlt@gmail.com>
2022-09-19 22:23:34 +0400

    добавил лекцию 6

--- Lesson_6/Program.cs
+++ Lesson_6/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача увеличить каждую из координат в 2 раза
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"
+            .Replace("(", "") //позволяет указать какие элементы в строке нужно не учитывать и на что их заменить
+            .Replace(")", "")
+            ;
+Console.WriteLine(text);
+
+var data = text.Split(" ") //позволяет разбить данные на массив - например первое число будут х, а второе у
+            .Select(item => item.Split(','))
+            .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+            .Where(e => e.x % 2 == 0)
+            .Select(point => (point.x * 10, point.y))
+            .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+    //Console.WriteLine();
+}
